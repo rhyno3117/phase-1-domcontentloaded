4b5410b661d42e4c0a92e9bd05cde8102ff826d0
Ryan Meyers <rhynogolfer42@gmail.com>
2023-08-05 23:30:38 -0600

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+//Adds an event listener to the document objecthe event being listened to is "DOMContentLoaded," 
+//which fires when the HTML document has been fully loaded and parsed. 
+//Once this event occurs, the provided function will be executed.
+document.addEventListener("DOMContentLoaded", function () {
+    updateDOM()
+});
+
+
+//makes function
+//grabs the Id which is text
+//replaces text with new text
+function updateDOM() {
+    document.getElementById("text")
+        .innerHTML = "This is really cool!";
+}
